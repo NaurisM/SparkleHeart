8975cb86dc312f71b618cd49423f23939094d6b5
NaurisM <nauris.magone@gmail.com>
2020-11-13 18:39:38 +0200

    slider update, cleaning

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,11 +1,4 @@
 
-
-// new Glide('.glide').mount()
-
-// new Glide('.glide', {
-//   type: 'slider'
-// }).mount()
-
 document.addEventListener( 'DOMContentLoaded', function () {
   new Splide( '.splide', {
     type   : 'loop',
