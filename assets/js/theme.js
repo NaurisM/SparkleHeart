e25e9cf6a91901210e05aec826a80365af6ba80a
NaurisM <nauris.magone@gmail.com>
2020-11-23 20:02:26 +0200

    fixed nav - remove body scroll

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,11 +1,11 @@
 (function () {
    const burger = document.querySelector('.burger');
    const nav = document.querySelector('.nav');
+   const body = document.querySelector('body');
 
    burger.addEventListener('click', () => {
+      body.classList.toggle('fixed'),
       burger.classList.toggle('burger__active'),
          nav.classList.toggle('show');
    });
 }());
-
-
